be921662810a18127958177eb3b82188b5b55e1d
poyrazemun <ofatih-poyraz@hotmail.com>
2022-06-27 21:49:41 +0200

    deneme

--- src/test/java/com/merve/runners/CukesRunner.java
+++ src/test/java/com/merve/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/merve/step_definitions",
-        tags = "@smoke"
+        tags = ""
 )
 
 public class CukesRunner {
